3c260c8dfc41bf19e249a91f014afba0da290744
Arley Rivas G <arivas@innside.co>
2022-09-14 00:01:09 -0500

    Formularios dinamicos con FormArray

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  {
+  
+  miFormulario : FormGroup = this.formBuilder.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.formBuilder.array([
+      [ 'Messi', Validators.required ], //no son arrays son colecciones de form controls
+      [ 'Iniesta', Validators.required ],
+    ], Validators.required)
+  });
 
-  constructor() { }
+  nuevoFavorito: FormControl = this.formBuilder.control('', Validators.required);
+
+  get favoritosArray(){
+    //le decimos que todo eso es un formArray
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  campoNoEsValido(campo: string) {
+    return this.miFormulario.controls[campo].errors 
+            && this.miFormulario.controls[campo].touched;
+  }
+
+  agregarFavorito(){
+    if( this.nuevoFavorito.invalid ){ return; }
+
+    //ya que los FormArray son controls, le podemos añadir  el nuevo favorito a través del get favoritosArray.push 
+    // this.favoritosArray.push( new FormControl( this.nuevoFavorito.value, Validators.required ) );  //opcion 1
+    this.favoritosArray.push( this.formBuilder.control( this.nuevoFavorito.value, Validators.required ) );     //opcion 2
+    
+    this.nuevoFavorito.reset();
+  }
+
+  borrarFavorito(indice: number){
+    this.favoritosArray.removeAt(indice);
+  }
+
+
+  guardar(){
+
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
 }
